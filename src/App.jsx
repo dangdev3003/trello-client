1776227f0de0627a4000f479b715e38aaaa6e91e
dangdev3003 <dangdev3003@gmail.com>
2022-03-23 10:14:03 +0700

    design trello front-end

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="trello-container">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://st2.depositphotos.com/3834629/5913/v/380/depositphotos_59131923-stock-illustration-wild-boar.jpg?forcejpeg=true"
+                alt="picture column"
+              />
+              Title: Dang dev
+            </li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://st2.depositphotos.com/3834629/5913/v/380/depositphotos_59131923-stock-illustration-wild-boar.jpg?forcejpeg=true"
+                alt="picture column"
+              />
+              Title: Dang dev
+            </li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://st2.depositphotos.com/3834629/5913/v/380/depositphotos_59131923-stock-illustration-wild-boar.jpg?forcejpeg=true"
+                alt="picture column"
+              />
+              Title: Dang dev
+            </li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://st2.depositphotos.com/3834629/5913/v/380/depositphotos_59131923-stock-illustration-wild-boar.jpg?forcejpeg=true"
+                alt="picture column"
+              />
+              Title: Dang dev
+            </li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://st2.depositphotos.com/3834629/5913/v/380/depositphotos_59131923-stock-illustration-wild-boar.jpg?forcejpeg=true"
+                alt="picture column"
+              />
+              Title: Dang dev
+            </li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+            <li>add what you'd like to work on below</li>
+          </ul>
+          <footer>add another card</footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
